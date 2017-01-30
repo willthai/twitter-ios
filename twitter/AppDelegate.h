1dd616c4046e0da33bbf24ad1bc8f18806145a2f
William Thai <wthai1027@gmail.com>
2017-01-30 10:24:43 -0800

    Initial Commit

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by William Thai on 1/30/17.
+//  Copyright © 2017 Y.CORP.YAHOO.COM\willthai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twitter
+//
+//  Created by William Thai on 1/30/17.
+//  Copyright © 2017 Y.CORP.YAHOO.COM\willthai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
